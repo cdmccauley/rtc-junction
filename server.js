2b1db9139864d2094b6f165c647ab6c21ab69b86
Chris McCauley <cdmccauley@gmail.com>
2019-12-21 12:38:55 -0700

    server tested working

--- server.js
+++ server.js
@@ -12,9 +12,107 @@ const server = express()
 
 const wss = new Server({ server });
 
+var users = {};
+
 wss.on('connection', (ws) => {
   console.log('Client connected');
-  ws.on('close', () => console.log('Client disconnected'));
+
+  ws.on('message', (message) => {
+    var data;
+    try {
+      data = JSON.parse(message);
+    } catch (e) {
+      console.log('invalid json');
+      data = {};
+    }
+    switch(data.type) {
+      case 'login':
+        console.log('user login: ', data.name);
+        if (users[data.name]) {
+          sendTo(ws, {
+            type: 'login',
+            success: false
+          });
+        } else {
+          users[data.name] = ws;
+          ws.name = data.name;
+          sendTo(ws, {
+            type: 'login',
+            success: true
+          });
+        }
+        break;
+      case 'offer':
+        console.log('sending offer to: ', data.name);
+        var conn = users[data.name];
+        if(conn != null) {
+          ws.otherName = data.name;
+          sendTo(conn, {
+            type: 'offer',
+            offer: data.offer,
+            name: ws.name
+          });
+        }
+        break;
+      case 'answer':
+        console.log('sending answer to: ', data.name);
+        var conn = users[data.name];
+        if(conn != null) {
+          ws.otherName = data.name;
+          sendTo(conn, {
+            type: 'answer',
+            answer: data.answer
+          });
+        }
+        break;
+      case 'candidate':
+        console.log('sending candidate to: ', data.name);
+        var conn = users[data.name];
+        if(conn != null) {
+          sendTo(conn, {
+            type: 'candidate',
+            candidate: data.candidate
+          });
+        }
+        break;
+      case 'leave':
+        console.log('disconnecting from: ', data.name);
+        var conn = users[data.name];
+        conn.otherName = null;
+        if(conn != null) {
+          sendTo(conn, {
+            type: 'leave'
+          });
+        }
+        break;
+      default:
+        sendTo(ws, {
+          type: 'error',
+          message: 'command not found: ' + data.type
+        });
+        break;
+    }
+  });
+
+  ws.on('close', () => {
+    console.log('Client disconnected')
+    if(ws.name) {
+      delete users[ws.name];
+      if(ws.otherName) {
+        console.log('disconnecting from: ', connection.otherName);
+        var conn = users[ws.otherName];
+        conn.otherName = null;
+        if(conn != null) {
+          sendTo(conn, {
+            type: 'leave'
+          });
+        }
+      }
+    }
+  });
+
+  ws.send('hello world');
+
 });
 
 setInterval(() => {
@@ -23,3 +121,6 @@ setInterval(() => {
   });
 }, 1000);
 
+function sendTo(connection, message) {
+  connection.send(JSON.stringify(message));
+}
